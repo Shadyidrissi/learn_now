23fc47380df81cf3085689bd528213550696e253
Shady Idrissi <chadiidac@gmail.com>
2025-11-13 23:39:33 +0100

    loading page

--- code/client/src/app/page.js
+++ code/client/src/app/page.js
@@ -2,7 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-    </>
+    <div id="home-loading">
+      <div id="home-loading-text">
+        <h4>
+          lore mmcvervieop eoirvn0eirhv eiruhvevb erubnvieu 9ere reru ger egierhgi erge rgervnoer ergeurhbvier ierheiurghiergh
+          uerhehrg erugherig erug erg erigh erigy erigh er9igheriuergh ehrigerigu er
+          vejrn iui ergeirhgeir bergergeirg erg e9gh e9g9eg rg 9er gergergbEFBWAUOFQW FGF8QWO7FG
+        </h4>
+        <button>Let's Start</button>
+      </div>
+      <img id="home-loading-image" src="https://www.21kschool.com/ua/wp-content/uploads/sites/6/2024/09/Learning-Methods.png" alt="" />
+    </div>
   );
 }
