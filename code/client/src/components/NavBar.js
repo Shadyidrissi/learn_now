a51211da572454514f4e046446ec246e1cd22687
Shady Idrissi <chadiidac@gmail.com>
2025-11-13 23:08:08 +0100

    start : create nav bar

---
+++ code/client/src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <div id='navbar'>
+            <a href="" id='navbar-logo'><span id='logo-learn-navbar'>Learn</span>Now</a>
+            <ul id='navbar-list'>
+                <a href="">hellow</a>
+                <a href="">ccc</a>
+                <a href="">wcwec</a>
+                <a href="">ewce</a>
+            </ul>
+            <ul id='navbar-list'>
+                <button>Login / Regeister</button>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
